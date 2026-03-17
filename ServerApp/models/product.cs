137d0728cb5e99f80e990fa32d426e6b2e7d869e
Miarahlff <133676475+Miarahlff@users.noreply.github.com>
2026-03-17 19:55:51 +0100

    Add files via upload

---
+++ ServerApp/models/product.cs
@@ -0,0 +1,17 @@
+namespace ServerApp.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Stock { get; set; }
+    public Category Category { get; set; }
+}
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
